19886df86a1ffe0b277ae8772852202f2ada6a42
AndreySouza02 <105511679+AndreySouza02@users.noreply.github.com>
2022-10-18 21:21:35 -0300

    Mapa

--- js/script.js
+++ js/script.js
@@ -5,16 +5,17 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 L.marker([-23.09238, -47.214721]).addTo(map)
-    .bindPopup('Augusto de Olveira Camargo: <a href="http://historiadeindaiatuba.blogspot.com/2020/05/1-curiosidade-sobre-historia-de.html" class="speciallink">Clique aqui</a>');
+    .bindPopup('Augusto de Olveira Camargo: <a href="#" class="speciallink">Clique aqui</a>');
 
 
-L.marker([-23.093747, -47.214603]).addTo(map)
-    .bindPopup('Francisco de Paula Leite: <a href="http://historiadeindaiatuba.blogspot.com/2020/07/os-segredos-dos-tumulos-ecleticos-de.html" class="speciallink">Clique aqui</a>');  
+L.marker([-23.114746, -47.221695]).addTo(map)
+    .bindPopup('Francisco de Paula Leite: <a href="#" class="speciallink">Clique aqui</a>');  
     
-    
+L.marker([-23.084272, -47.224538]).addTo(map)
+    .bindPopup('Ruy Ohtake: <a href="#" class="speciallink">Clique aqui</a>');      
 
-    // function onMapClick(e) {
-    //     alert("You clicked the map at " + e.latlng);
-    // }
+    //  function onMapClick(e) {
+    //      alert("You clicked the map at " + e.latlng);
+    //  }
     
-    // map.on('click', onMapClick);
+    //  map.on('click', onMapClick);
